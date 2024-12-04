5a5c5bcd429a035b1e7c1190d18cce1d232c2d63
a-star711 <a.krasimirov35@gmail.com>
2024-12-04 16:07:08 +0200

    Add Update,Delete & Patch logic with mongoose queries

--- task-manager-app/controllers/tasks.js
+++ task-manager-app/controllers/tasks.js
@@ -1,34 +1,67 @@
 
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res) => {
-  res.send('All Items from the file')
+const getAllTasks = async (req,res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({tasks})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
 const createTask = async (req,res) => {
   try {
     const task = await Task.create(req.body)
     res.status(201).json({task})
-    
+
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
+}
+
+const getSingleTask = async (req,res) => {
+  try {
+    const { id:taskID } = req.params
+    const task = await Task.findOne({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg: `Task with ${taskID} doesn't exist`})
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
+}
+
+const updateTask = async (req,res) => {
+  try {
+    const {id:taskID} = req.params;
+    const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+      new:true,runValidators:true,
+    })
+    if(!task){
+      return res.status(404).json({msg: `Task with ${taskID} doesn't exist`})
+    }
+
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
+}
+
+const deleteTask = async (req,res) => {
+  try {
+    const { id:taskID } = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(!task){
+      return res.status(404).json({msg: `Task with ${taskID} doesn't exist`})
+    }
+    res.status(200).json({task})
   } catch (error) {
     res.status(500).json({msg:error})
   }
 
 }
-
-const getSingleTask = (req,res) => {
-  res.json({id:req.params.id})
-}
-
-const updateTask = (req,res) => {
-  res.json({id:req.params.id})
-  res.send('Task has been updated')
-}
-
-const deleteTask = (req,res) => {
-  res.send('Task has been deleted')
-}
-
 
 module.exports = {
   getAllTasks,
